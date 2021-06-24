2ed5bf4156512b1285751b6ca5ae08a916e644a4
projjal1 <gopprojjal@gmail.com>
2021-06-24 22:03:43 +0530

    Added UI changes and added simlarity images in home tab

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.operators import distinct_op
 from PIL import Image
 import random
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import TruncatedSVD
+import pickle
+from collections import Counter
+from datetime import datetime
 
 # Flask app configs
 app=Flask(__name__)
@@ -39,14 +45,202 @@ class pin_category(db.Model):
     id = db.Column('category_id', db.Integer, primary_key=True)
     name = db.Column(db.String(200))
 
+class track_visits(db.Model):
+    id = db.Column('visit_id', db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    img_id = db.Column(db.Integer)
+
+class admin_post(db.Model):
+    id = db.Column('admin_id', db.Integer, primary_key=True)
+    advertisement_title = db.Column(db.String(100))
+    thought_title = db.Column(db.String(100))
+    advertisement_link = db.Column(db.String(100))
+    thought_link = db.Column(db.String(100))
+    date = db.Column(db.String(100))
+
+#Run only to create initial .sqlite database 
 #db.create_all()
+
+
+#Data extractors 
+def extract_users():
+    records=users.query.all()
+    id,email,interests=[],[],[]
+    for rec in records:
+        var_id,var_name=rec.id,rec.email
+        for interest in rec.interests.split(","):
+            id.append(var_id)
+            email.append(var_name)
+            interests.append(interest)
+
+    df=pd.DataFrame({"user_id":pd.Series(id),"name":pd.Series(email),"category":pd.Series(interests),"flag":pd.Series(np.ones(len(email)))})
+    return df
+
+def extract_images():
+    records=images.query.all()
+    id,title,fields,user_ids=[],[],[],[]
+    for rec in records:
+        id.append(rec.id)
+        title.append(rec.title)
+        fields.append(rec.fields)
+        user_ids.append(rec.user_id)
+
+    df=pd.DataFrame({"img_id":pd.Series(id),"title":pd.Series(title),"category":pd.Series(fields),"user_id":pd.Series(user_ids),"img_flag":pd.Series(np.ones(len(title)))})
+    return df
+
+def extract_tracks():
+    records=track_visits.query.all()
+    id,user_ids,img_ids=[],[],[]
+    for rec in records:
+        id.append(rec.id)
+        user_ids.append(rec.user_id)
+        img_ids.append(rec.img_id)
+
+    df=pd.DataFrame({"visit_id":pd.Series(id),"user_id":pd.Series(user_ids),"img_id":pd.Series(img_ids),"visits":pd.Series(np.ones(len(id)))})
+    return df 
+
+def extract_category():
+    records=pin_category.query.all()
+    id,name=[],[]
+    for rec in records:
+        id.append(rec.id)
+        name.append(rec.name)
+
+    df=pd.DataFrame({"cat_id":pd.Series(id),"category":pd.Series(name)})
+    return df
+
+#calls to compute ratings crosstab matrix and perform Pearson Correlation
+#user-user similarity find out
+def compute_user_similarity():
+    user_data=extract_users()
+    image_data=extract_images()
+    track_data=extract_tracks()
+    category_data=extract_category()
+
+    #user_merge_category=pd.merge(user_data[["name","category"]],category_data[["category"]],on="category")
+    user_merge_category=pd.merge(user_data[["name","category","flag"]],category_data[["category"]],on="category")
+    user_category = user_merge_category.pivot_table(values='flag', index='name', columns='category', fill_value=0)
+    combined_dataset=pd.merge(user_data[["user_id","name"]],image_data[["img_id","user_id","category","img_flag"]],on="user_id")
+    user_images = combined_dataset.pivot_table(values= 'img_flag', index='name', columns='category', fill_value=0)
+    track_images=pd.merge(image_data[['img_id','title']],track_data[['img_id','user_id']],on="img_id")
+    track_user_images=pd.merge(track_images,user_data[["user_id","name"]],on="user_id")
+
+    final_set=pd.merge(user_category,user_images,on="name")
+    from sklearn.decomposition import TruncatedSVD
+
+    #generate SVD matrix
+    SVD = TruncatedSVD(n_components=5, random_state=17)
+    resultant_matrix = SVD.fit_transform(final_set)
+    #Computing similarity scores
+    corr_mat = np.corrcoef(resultant_matrix)
+    import pickle
+    # save array
+    with open('data-mining/user-corr.pkl','wb') as file:
+        pickle.dump(corr_mat,file)
+    # save track_data
+    with open('data-mining/track_data.pkl','wb') as file:
+        pickle.dump(track_user_images,file)
+    # save final set 
+    with open("data-mining/final_set.pkl",'wb') as file:
+        pickle.dump(final_set,file)
+
+#compute_user_similarity()
+#Function to return images based on user-user collaborative filtering
+def call(nm):
+    with open('data-mining/final_set.pkl','rb') as file:
+        final_set=pickle.load(file)
+    with open('data-mining/user-corr.pkl','rb') as file:
+        corr_mat=pickle.load(file)
+    with open('data-mining/track_data.pkl','rb') as file:
+        track_user_images=pickle.load(file)
+
+    threshold=0.3
+    user_list=list(final_set.index)
+
+    idx=user_list.index(nm)
+    user_select=Counter()
+    
+    itr=0
+    for score in corr_mat[idx]:
+        if score>0.98 or score<threshold:
+            itr+=1
+        else:
+            user_select[itr]=score
+            itr+=1
+
+    #Common users
+    common_users=5    #Change it to generate more data
+
+    image_names,ids=[],[]
+    for k,u in user_select.most_common(common_users):
+        email=user_list[k]
+        row=track_user_images[track_user_images["name"]==email]
+        
+        img_names=row.title.values
+        id_imgs=row.img_id.values
+        
+        for i in range(len(img_names)):
+            nm=img_names[i]
+            idsers=id_imgs[i]
+            if nm in image_names:
+                continue 
+            else:
+                image_names.append(nm)
+                ids.append(idsers)
+
+    ls_objs=[]
+    for var in ids:
+        print(id,images.query.filter_by(id=str(var)).all())
+        ls_objs.append(images.query.filter_by(id=str(var)).all()[0])
+    return ls_objs
+
+#Admin post 
+@app.route("/admin_post",methods=["GET","POST"])
+def admin_listing():
+    if request.method=="GET":
+        return render_template("admin_post.html",display_nm="Admin")
+    else:
+        advertisment_title=request.form.get('iname2')
+        advertisment_link=request.form.get('link2')
+        thought_title=request.form.get('iname1')
+        thought_link=request.form.get('link1')
+        img_path_adv=request.files['img2']
+        img_path_thought=request.files['img1']
+
+        #Creating date-string 
+        dt=datetime.now()
+        format=dt.strftime("%d %B, %Y")
+
+        admin_obj=admin_post(advertisement_link=advertisment_link,advertisement_title=advertisment_title,thought_title=thought_title,thought_link=thought_link,date=format)
+        db.session.add(admin_obj)
+        db.session.commit()
+
+
+        img1=Image.open(img_path_adv)
+        img2=Image.open(img_path_thought)
+
+        #retreive id 
+        admin_rec=admin_post.query.filter_by(date=format).all()
+        admin_rec=admin_rec[0]
+        admin_id=admin_rec.id
+        
+        img1.save('static/admin_promo/adv'+str(admin_id)+".jpg")
+        img2.save('static/admin_promo/thought'+str(admin_id)+".jpg")
+
+        return redirect(url_for('index'))
+
 
 #Routing functions
 @app.route('/', defaults={'category': "All"})
 @app.route('/<category>')
 def index(category="All"):
-    if category=="All":
+    if category=="All" and "email" not in session:
         record_images=images.query.all()
+    elif category=="All" and "email" in session:
+        if session["username"]!="Admin":
+            record_images=call(session["email"])
+        else:
+            record_images=images.query.all()[-5:]
     else:
         record_images=images.query.filter_by(fields=category).all()
 
@@ -58,7 +252,16 @@ def index(category="All"):
         return render_template("home.html", display_nm="Author",images_list=record_images)
 
 
-#TODO Return Similar images to shown image
+@app.route('/today')
+def exclusive():
+    if "username" in session:
+        nm=session["username"]
+    else:
+        nm="Author"
+    
+    listing_images=admin_post.query.all()[::-1]
+    return render_template("latest_uploaded.html",display_nm=nm,images_list=listing_images)
+
 
 @app.route('/view/<photo_id>')
 def view_photo(photo_id):
@@ -66,13 +269,18 @@ def view_photo(photo_id):
         nm="Author"
     else:
         nm=session["username"]
+        if nm!="Admin":
+            #Track visit 
+            user_visited=users.query.filter_by(email=session["email"]).all()
+            visit_obj = track_visits(user_id=user_visited[0].id, img_id=photo_id)
+            db.session.add(visit_obj)
+            db.session.commit()
     
-
-
     record_images=images.query.filter_by(id=photo_id).all()
     user_id=record_images[0].user_id
     user_details=users.query.filter_by(id=user_id).all()
-    img_obj=images.query.filter_by(user_id=user_id).all()
+    img_obj=images.query.filter_by(fields=record_images[0].fields).all()
+    img_obj.pop(img_obj.index(record_images[0]))
     random.shuffle(img_obj)
 
     return render_template("view-photo.html",display_nm=nm, img_data=record_images[0], user_data=user_details[0], 
@@ -84,7 +292,7 @@ def profile():
 
     if request.method=="GET":
         return render_template("profile.html",display_nm=nm)
-    elif request.method=="POST":
+    elif request.method=="":
         #Get input infos
         bio=request.form.get("bio")
         website=request.form.get("url")
@@ -108,6 +316,9 @@ def profile():
         db.session.add(obj)
         db.session.commit()
 
+        #Call function to generate updated ratings crosstab
+        compute_user_similarity()
+
         return redirect(url_for('index'))
 
 
@@ -123,6 +334,8 @@ def profile_view(profile_email,action):
             return render_template("sign.html",display_nm=nm)
         else:
             return render_template("sign.html",display_nm=nm,error="You need to log in to view profile of users.")
+    elif session["username"]=="Admin":
+        return redirect(url_for('index'))
     else:
         nm=session["username"]
         
@@ -167,12 +380,14 @@ def post():
         db.session.add(image_obj)
         db.session.commit()
 
-
         #Fetch image-id 
         record=images.query.filter_by(title=title, description=desc, fields=field, links=links, user_id=user_id).all()
         image_id=record[0].id
         image=Image.open(img)
         image.save("static/portal_images/"+str(image_id)+".jpg")
+
+        #Updated ratings crosstab matrix
+        compute_user_similarity()
 
         cat_records=pin_category.query.all()
         return render_template("post.html",display_nm=nm, success="Image Saved",categories=cat_records)
@@ -228,8 +443,16 @@ def login():
     elif request.method == "POST":
         email=request.form.get("email")
         pwd=request.form.get("pwd")
+
+
+        #Login explicitly stated
+        if email=="administrator@pinterest.com" and pwd=="admin":
+            session["email"]="administrator@pinterest.com"
+            session["username"]="Admin"
+            return redirect(url_for("index"))
+
         record=users.query.filter_by(email=email, password=pwd).all()
-        
+
         if len(record)>0:
             full_name=record[0].name
             session["username"] = full_name
